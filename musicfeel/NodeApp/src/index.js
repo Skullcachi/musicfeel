be9d5b118e4b880a7fda3b2ffbf67147bce41d5f
Pablo Pineda <pablo12.izq@gmail.com>
2019-07-01 17:48:47 -0600

    Added Rekognition

--- musicfeel/NodeApp/src/index.js
+++ musicfeel/NodeApp/src/index.js
@@ -1,28 +1,8 @@
-<<<<<<< HEAD
-var AWS = require('aws-sdk');
-var params = {
-    Attributes: ["ALL"],
-    Image: {
-     S3Object: {
-      Bucket: "musicfeel", 
-      Name: "happy-girl-field.jpg"
-     }
-    }
-   };
-   AWS.config.update({region:'us-east-1'});
-   var rekognition = new AWS.Rekognition();
-   rekognition.detectFaces(params, function(err, data) {
-    if (err) console.log(err, err.stack); // an error occurred
-    else     console.log(data);           // successful response
-  });
-=======
 const express = require('express');
 const path = require('path');
 const morgan = require('morgan');
 const mysql = require('mysql');
 const myConnection = require('express-myconnection');
-const cors = require('cors');
-
 
 const app = express();
 var cors = require('cors');
@@ -30,6 +10,7 @@ var cors = require('cors');
 //Importando Rutas
 
 const userRuotes = require('./routes/usuario');
+const rekognitionRoute = require('./routes/rekognition');
 
 
 app.set('port', process.env.PORT || 3000);
@@ -38,13 +19,13 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(cors());
 app.use(morgan('dev'));
-app.use(myConnection(mysql, {
+/*app.use(myConnection(mysql, {
     host: 'localhost',
     user: 'root',
     password: '',
     port: 3306,
     database: 'musicFeelLocal'
-}, 'single'));
+}, 'single'));*/
 
 app.use(cors());
 
@@ -52,6 +33,7 @@ app.use(express.urlencoded({extended: false}));
 
 //Rutas 
 app.use('/', userRuotes);
+app.use('/rekognition/', rekognitionRoute);
 
 //staticFiles
 app.use(express.static(path.join(__dirname, 'public')));
@@ -60,4 +42,3 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.listen(app.get('port'), () => {
     console.log('Server Working! on Port 3000');
 });
->>>>>>> master
