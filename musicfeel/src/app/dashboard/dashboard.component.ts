74c17d4ded57bb165b3f84b938a593e4f0631a0d
Sergio Molina <molinasergio.delta@gmail.com>
2019-07-04 10:16:26 -0600

    made a lot of corrections, improved UI, working spotify service, project almost done!

--- musicfeel/src/app/dashboard/dashboard.component.ts
+++ musicfeel/src/app/dashboard/dashboard.component.ts
@@ -20,6 +20,7 @@ export class DashboardComponent implements OnInit {
 
   ngOnInit() {
     this.getWeather();
+    this.getHistory();
   }
 
   ngAfterViewInit()
@@ -36,6 +37,7 @@ export class DashboardComponent implements OnInit {
   {
     this.weatherService.getCurrentWeather().subscribe((res)=>{
       console.log(res["weather"]);
+      localStorage.setItem("weather", res["weather"]);
       let currentWeather = document.getElementById("weather-value");
       currentWeather.innerHTML = res["weather"];
     }, (err) => {
@@ -46,7 +48,17 @@ export class DashboardComponent implements OnInit {
   {
     let user_id = localStorage.getItem("user_id");
     this.recommendationService.getHistory(user_id).subscribe((res)=>{
-      
+      let response = res["recomendaciones"];
+      console.log(res['recomendaciones']);
+      if (response.length > 0)
+      {
+        let recommended = res['recomendaciones'];
+        this.rows = recommended;
+      }
+      else
+      {
+        this.rows = [];
+      }
     }, (err) => {
       console.log(err);
     });
@@ -57,13 +69,55 @@ export class DashboardComponent implements OnInit {
     this.route.navigate(['uploadImage'])
   }
 
+  rows = [];
+  getRecommendations()
+  {
+    this.recommendationService.getHistory(localStorage.getItem("user_id")).subscribe((res)=> {
+      console.log(res);
+      let recommended = res[0];
+      let cells = [];
+      for(var i =0; i <= recommended.length - 1 ; i++){
+        cells.push(recommended.id);
+        cells.push(recommended.name);
+        cells.push(recommended.album);
+        cells.push(recommended.href);
+        cells.push(recommended.external_url);
+      }
+      
+      console.log("alskdfjl");
+      this.rows.push(cells);
+      console.log(this.rows);
+    })
+  }
+
   requestPermission()
   {
-    var scopes = 'user-read-private user-read-email';
+    window.open(`https://accounts.spotify.com/authorize?response_type=token&client_id=7c2e7bd93be84d6abd457e43f2152860&scope=user-read-private%20user-read-email&redirect_uri=${encodeURIComponent("http://localhost:4200/spotify")}&state=${this.generateRandomString(16)}`, "_blank");
+
+    /* this.recommendationService.getPermission().subscribe((res)=> {
+      console.log(res);
+    }, (err)=> {}); */
+    /* var scopes = 'user-read-private user-read-email';
     window.location.replace('https://accounts.spotify.com/authorize' +
       '?response_type=code' +
       '&client_id=' + "7c2e7bd93be84d6abd457e43f2152860" +
       (scopes ? '&scope=' + encodeURIComponent(scopes) : '') +
-      '&redirect_uri=' + encodeURIComponent("http://localhost:3000/spotify"));
+      '&redirect_uri=' + encodeURIComponent("http://localhost:3000/spotify")); */
+  }
+
+  private generateRandomString(length) {
+    var text = '';
+    var possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  
+    for (var i = 0; i < length; i++) {
+      text += possible.charAt(Math.floor(Math.random() * possible.length));
+    }
+    return text;
+  };
+
+  logOut()
+  {
+    localStorage.clear();    
+    this.route.navigate(['login'])
   }
 }
