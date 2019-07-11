151a08231ba1aa10de362c9b42113f9b9dd6b4ae
Sergio Molina <molinasergio.delta@gmail.com>
2019-07-10 18:45:31 -0600

    changed spotify endpoint to bucket endpoint

--- musicfeel/src/environments/environment.ts
+++ musicfeel/src/environments/environment.ts
@@ -13,7 +13,8 @@ export const environment = {
   /* CRUDAPIEndpoint: 'http://ec2-52-14-238-78.us-east-2.compute.amazonaws.com:3000/',
   RekognitionAPIEndpoint: 'http://ec2-52-14-238-78.us-east-2.compute.amazonaws.com:3000/',
   fileUploadAPIEndpoint: 'http://ec2-52-14-238-78.us-east-2.compute.amazonaws.com:3000/',*/
-  SpotifyAPIEndpoint: 'http://ec2-52-14-238-78.us-east-2.compute.amazonaws.com:4200/'
+  SpotifyAPIEndpoint: 'http://musicfeel-frontend.s3-website.us-east-2.amazonaws.com:4200/'
+  /* SpotifyAPIEndpoint: 'http://ec2-52-14-238-78.us-east-2.compute.amazonaws.com:4200/' */
   /* SpotifyAPIEndpoint: 'http://localhost:4200/' */
 };
 
